8d52438e3d7efd2b6e67104e8c8cead90a7c3fd2
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-10-08 06:15:48 +0000

    [Create] 今日のイベント一覧カウント

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,19 @@
 class Public::HomesController < ApplicationController
+  
   def top
-    @events = Event.all
+    @event = Event.new #新しくイベント登録用
+    @search_event = Event. #イベント検索用
+    #当日の開催イベント一覧取得
+    d = Date.today
+    @year = d.year.to_s
+    @month = d.month.to_s
+    @day = d.day.to_s
+    @today_events = Event.where(hold_year: @year).where(hold_month: @month).where(hold_day: @day)
+  end
+  
+  
+  
+  def top_params
+      params.require(:top).permit(:name, :amount)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  Event.create!(
+   name: '桑原由気と本渡楓のパリパリパーリィ☆ 6th Anniversary Greeting Parry',
+   hold_year: '2022',
+   hold_month: '12',
+   hold_day: '30',
+   hold_place: 'サイエンスホール'
+)
+  Event.create!(
+   name: '『ゆうか・ゆうきのAll Around search』番組公開収録イベント',
+   hold_year: '2023',
+   hold_month: '04',
+   hold_day: '23',
+   hold_place: '文化放送メディアプラスホール'
+)
