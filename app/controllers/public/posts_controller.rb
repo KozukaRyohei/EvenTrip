73a354282408332caad57e298a13c30405a85d32
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2023-10-11 07:10:34 +0000

    コメント機能実装

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -16,6 +16,7 @@ class Public::PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @comment = PostComment.new
   end
 
   private
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
 
-  belongs_to:user
+  belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
   has_many_attached :post_images
 
   def favorited_by?(user)
