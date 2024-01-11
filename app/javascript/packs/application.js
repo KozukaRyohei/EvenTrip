24afd298250e8ff289be7f88ebdec84b091e0710
KozukaRyohei <138641952+KozukaRyohei@users.noreply.github.com>
2024-01-11 15:08:17 +0000

    [delete] events.scss

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,6 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
-import "../stylesheets/events";
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
